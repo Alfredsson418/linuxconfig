1e13a102ab86e8ddefd4eb7a1e39de1e1c1a915b
Oskar Alfredsson <oskar.alfredsson@proton.me>
2025-04-14 20:33:53 +0200

    Added starting config

---
+++ copy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+home_dir="$HOME/"
+target_dir="./dotfiles"
+
+dot_files=(".bashrc")
+
+for i in "${dot_files[@]}"; do
+    dir=$(readlink -f "$home_dir$i")
+    target="$target_dir"
+    mkdir -p $target
+    echo "Copying from $dir to $target"
+    cp -r "$dir" "$target_dir"
+done
+
+config_dir="$home_dir.config/"
+
+config_dirs=("sway" "swaync" "waybar" "rofi" "kitty")
+
+for i in "${config_dirs[@]}"; do
+    dir=$(readlink -f "$config_dir$i")
+    target="$target_dir/.config"
+    mkdir -p $target
+    echo "Copying from $dir to $target"
+    cp -r "$dir" "$target"
+done
