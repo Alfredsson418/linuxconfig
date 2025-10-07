1e1266519085d70064fdc065e87b72b15d2bac74
Oskar Alfredsson <oskar.alfredsson@proton.me>
2025-10-07 09:06:24 +0200

    Added musicplayer script too show spotify current song

--- dotfiles/.config/nvim/lua/plugins/config/tree.lua
+++ dotfiles/.config/nvim/lua/plugins/config/tree.lua
@@ -1,13 +1,14 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("nvim-tree").setup({
-      sort = { sorter = "case_sensitive" },
-      view = { width = 30},
-      renderer = { group_empty = true },
-      filters = { dotfiles = false },
-    })
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("nvim-tree").setup({
+      	sort = { sorter = "case_sensitive" },
+      	view = { width = 30},
+      	renderer = { group_empty = true },
+      	filters = { dotfiles = false },
+		git = { enable = true, ignore = false, timeout = 400 },
+	})
     end,
 }
 
