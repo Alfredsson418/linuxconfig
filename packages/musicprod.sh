a28685a7b86b8bd4ca93ff3467d0bb1bbdc34b77
Oskar Alfredsson <oskar.alfredsson@proton.me>
2026-01-09 15:04:05 +0100

    Added Comments to download paths

--- packages/musicprod.sh
+++ packages/musicprod.sh
@@ -1,11 +1,18 @@
 fedora_music_prod=(
-    "helm"
     "zynaddsubfx"
+	"zynaddsubfx-lv2"
 	"lsp-plugins"
+	"lsp-plugins-lv2"
 	"sfizz"
+	"qjackctl" # GUI for edit JACK
+	"pulseaudio-module-jack" # Module for jack and pulseaudio to work together
+	"ardour8"
 )
 
 
 flatpak_music_prod=(
     
 )
+
+# Download vital from https://vital.audio/#getvital
+# Download ardour from https://ardour.org/
